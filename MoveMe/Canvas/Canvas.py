a15502a448a958969da13a62bf4cb3f47271da06
Dmytro Konobrytskyi <dkonobr.cv@gmail.com>
2013-11-09 23:05:09 -0500

    Implemented nodes highlighting.

--- MoveMe/Canvas/Canvas.py
+++ MoveMe/Canvas/Canvas.py
@@ -16,13 +16,22 @@ class Canvas(wx.PyScrolledWindow):
                            self.canvasDimensions[0]/self.scrollStep, 
                            self.canvasDimensions[1]/self.scrollStep)
         
+        #This list stores all objects on canvas
         self._canvasObjects = [SimpleBoxNode([20,20]), SimpleBoxNode([140,40]), SimpleBoxNode([60,120])]
 
+        #References to objects required for implementing various operations
+        self._objectUnderCursor = None
+
+        #Rendering initialization
         self._dcBuffer = wx.EmptyBitmap(*self.canvasDimensions)
         self.Render()
         self.Bind(wx.EVT_PAINT, 
                   lambda evt: wx.BufferedPaintDC(self, self._dcBuffer, wx.BUFFER_VIRTUAL_AREA)
                   )
+        
+        self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnMouseLeftDown)
+        self.Bind(wx.EVT_LEFT_UP, self.OnMouseLeftUp)
 
     def Render(self):
         """Render all nodes and their connection in depth order."""
@@ -31,8 +40,31 @@ class Canvas(wx.PyScrolledWindow):
         dc = wx.BufferedDC(cdc, self._dcBuffer)
         dc.Clear()
         gc = wx.GraphicsContext.Create(dc)
-        
+
         for obj in self._canvasObjects:
             gc.PushState()
             obj.Render(gc)
             gc.PopState()
+
+        if self._objectUnderCursor:
+            gc.PushState()
+            self._objectUnderCursor.RenderHighlighting(gc)
+            gc.PopState()
+
+    def OnMouseMotion(self, evt):
+        pos = self.CalcUnscrolledPosition(evt.GetPosition()).Get()
+        self._objectUnderCursor = self.FindObjectUnderPoint(pos)
+        self.Render()
+
+    def OnMouseLeftDown(self, evt):
+        pass
+    def OnMouseLeftUp(self, evt):
+        pass
+
+    def FindObjectUnderPoint(self, pos):
+        #Check all objects on a canvas. Some objects may have multiple components and connections.
+        for obj in reversed(self._canvasObjects):
+            objUnderCursor = obj.ReturnObjectUnderCursor(pos)
+            if objUnderCursor: 
+                return objUnderCursor
+        return None
--- MoveMe/Canvas/Objects/SimpleBoxNode.py
+++ MoveMe/Canvas/Objects/SimpleBoxNode.py
@@ -18,3 +18,15 @@ class SimpleBoxNode(object):
                                 self.boundingBoxDimensions[1], 10)
         gc.SetFont(wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT))
         gc.DrawText("(%d, %d)"%(self.position[0], self.position[1]), self.position[0]+10, self.position[1]+10)
+
+    def RenderHighlighting(self, gc):
+        gc.SetBrush(wx.Brush('#888888', wx.TRANSPARENT))
+        gc.SetPen(wx.Pen('#888888', 3, wx.DOT))
+        gc.DrawRectangle(self.position[0]-3, self.position[1]-3, self.boundingBoxDimensions[0]+6, self.boundingBoxDimensions[1]+6)
+        
+    def ReturnObjectUnderCursor(self, pos):
+        if pos[0] < self.position[0]: return None
+        if pos[1] < self.position[1]: return None
+        if pos[0] > self.position[0]+self.boundingBoxDimensions[0]: return None
+        if pos[1] > self.position[1]+self.boundingBoxDimensions[1]: return None
+        return self
